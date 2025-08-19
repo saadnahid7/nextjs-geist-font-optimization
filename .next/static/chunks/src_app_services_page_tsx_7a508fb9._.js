ef14232a85b541bb650416fe7de8d5f988829b87
Saad Bin Abdul Hai <59447300+saadnahid7@users.noreply.github.com>
2025-08-18 21:40:16 -0400

    Add .next/static/chunks/src_app_services_page_tsx_7a508fb9._.js 1755567612508

---
+++ .next/static/chunks/src_app_services_page_tsx_7a508fb9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_0f23e523._.js"
+],
+    source: "dynamic"
+});
