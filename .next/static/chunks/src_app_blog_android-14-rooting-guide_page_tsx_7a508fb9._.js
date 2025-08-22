0022a2fda88378a31948b012ab5e78ff76f07ddc
Saad Bin Abdul Hai <59447300+saadnahid7@users.noreply.github.com>
2025-08-22 05:53:42 -0400

    Add .next/static/chunks/src_app_blog_android-14-rooting-guide_page_tsx_7a508fb9._.js 1755856418766

---
+++ .next/static/chunks/src_app_blog_android-14-rooting-guide_page_tsx_7a508fb9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_248187ed._.js",
+  "static/chunks/node_modules_@radix-ui_e00c01b8._.js"
+],
+    source: "dynamic"
+});
