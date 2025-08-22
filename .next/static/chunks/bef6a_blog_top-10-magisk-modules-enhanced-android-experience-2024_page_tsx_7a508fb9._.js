5632bcf62fc7ca8b332f86436a4b550febd1795e
Saad Bin Abdul Hai <59447300+saadnahid7@users.noreply.github.com>
2025-08-22 05:53:33 -0400

    Add .next/static/chunks/bef6a_blog_top-10-magisk-modules-enhanced-android-experience-2024_page_tsx_7a508fb9._.js 1755856409082

---
+++ .next/static/chunks/bef6a_blog_top-10-magisk-modules-enhanced-android-experience-2024_page_tsx_7a508fb9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8cc488dd._.js",
+  "static/chunks/node_modules_@radix-ui_e00c01b8._.js"
+],
+    source: "dynamic"
+});
